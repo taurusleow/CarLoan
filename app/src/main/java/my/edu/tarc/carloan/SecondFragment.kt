26ee8463de69195623e843bfa71e82cfcf554173
Ashley Lew <ashleylew@hotmail.com>
2022-08-01 03:00:10 +0800

    UI, Data binding

--- app/src/main/java/my/edu/tarc/carloan/SecondFragment.kt
+++ app/src/main/java/my/edu/tarc/carloan/SecondFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import my.edu.tarc.carloan.databinding.FragmentSecondBinding
 
@@ -13,6 +14,7 @@ import my.edu.tarc.carloan.databinding.FragmentSecondBinding
  */
 class SecondFragment : Fragment() {
 
+    //put thee value in when there the variable is not null
     private var _binding: FragmentSecondBinding? = null
 
     // This property is only valid between onCreateView and
@@ -24,13 +26,29 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        //_binding = FragmentSecondBinding.inflate(inflater,
+        // container,
+        // false)
+        //TODO 4: Initialise Data Binding Class
+        _binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_second,
+            container,
+            false
+        )
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //TODO 5: Create an instance of data set
+        val interestDataSet = InterestDataSet(
+            "CIMB\nMaybank\nPubic Bank\nRHB",
+            "2.95%(p.a)\n2.99%(p.a)\n3.05%(p.a)\n3.10%(p.a)")
+
+        //TODO 6: Connect data set to layout
+        binding.interestDataSet = interestDataSet
 
         binding.buttonSecond.setOnClickListener {
             findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
